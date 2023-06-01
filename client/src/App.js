93f017d93fc7a1f7c4febb96b6c7e01e134b070b
boris75213@gmail.com <you@example.com>
2023-06-01 20:23:41 +0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "leaflet/dist/leaflet.css";
+import "./app.css";
+import authService from "./Map/auth.service";
+import { Map } from "./Map/Map";
+import { Overspeed } from "./Map/Overspeed";
+import { MyComponent } from "./data/test3";
+// export const App = () => {
+//   return <Map />;
+// };
+function App() {
+  let [currentUser, setCurrentUser] = useState(authService.getCurrentUser());
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Map />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/overspeed" element={<MyComponent />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
